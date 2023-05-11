4bde39d72387cda5313d6a4c9995bc8b01105bf0
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-05-11 18:38:40 -0300

    functions reminder added

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 export default function TaskCard({ task, handleDelete, toogleReminder }) {
 
     return (
-        <div className='task-container' onDoubleClick={() => toogleReminder(task.id)}>
+        <div className={`task-container ${task.reminder ? 'task-reminder' : ''}`} onDoubleClick={() => toogleReminder(task.id)}>
             <h3>{task.text} <FaTimes onClick={() => handleDelete(task.id)}/></h3>
             <p>{task.day}</p>
         </div>
