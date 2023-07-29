7a4741e1e497e470384624357bdbbfcb07f1bda9
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-29 20:03:52 -0300

    update form for more details

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 export default function AddTask({ handleAdd }) {
   const [text, setText] = useState('')
   const [day, setDay] = useState('')
+  const [hour, setHour] = useState('')
   const [reminder, setReminder] = useState(false)
+  const [details, setDetails] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -15,31 +17,45 @@ export default function AddTask({ handleAdd }) {
       alert('please add a date')
       return
     }
-    handleAdd({text, day, reminder})
-    // console.log(text, day, reminder)
+    handleAdd({text, day, hour, details, reminder})
+    // console.log(text, day, hour, details, reminder)
     setText('')
     setDay('')
+    setHour('')
+    setDetails('')
     setReminder(false)
   }
 
   return (
     <form className='form-container' onSubmit={handleSubmit} >
-      <div>
+      <fieldset>
         <label>Task</label>
         <input type="text" placeholder='Meeting...' value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
-      {/* <div>
+      </fieldset>
+      {/* <fieldset>
           <label>Day & Time</label>
           <input type="datetime-local" value={day} onChange={(e) => setDay(e.target.value)} />
-      </div> */}
-      <div>
-        <label>Day & Time</label>
-        <input type="text" placeholder='June 20th at...' value={day} onChange={(e) => setDay(e.target.value)} />
-      </div>
-      <div>
-        <label>Reminder</label>
-        <input type="checkbox" checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
-      </div>
+      </fieldset> */}
+      <fieldset className='day-time'>
+        <div>
+          <label>Day</label>
+          <input type="text" placeholder='June 20th at...' value={day} onChange={(e) => setDay(e.target.value)} />
+        </div>
+        <div>
+          <label>Time</label>
+          <input type="text" placeholder='June 20th at...' value={hour} onChange={(e) => setHour(e.target.value)} />
+        </div>
+      </fieldset>
+      <fieldset>
+        <label>Details</label>
+        <input type="text" placeholder='Take the examples...' value={details} onChange={(e) => setDetails(e.target.value)} />
+      </fieldset>
+      <fieldset>
+        <label>
+          <input type="checkbox" checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+          Reminder
+        </label>
+      </fieldset>
       <button className='btn btn-save'>Save</button>
     </form>
   )
